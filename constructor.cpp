f14d060c498a4ac1d5c934c5790883aa698fcdb6
AayushKumar <www.aayuhkumar@gmail.com>
2019-02-01 22:48:27 +0530

    added copy constructor

--- constructor.cpp
+++ constructor.cpp
@@ -15,6 +15,11 @@ class student{
    age=a;
    strcpy(name,n);
    }
+
+   student(student &s){    //copy constructor
+   age=s.age;
+   strcpy(name,s.name);
+   }
    
    void display(){
    cout<<"name: "<<name;
@@ -29,4 +34,7 @@ student s2(18,"joe");
 s1.display();
 cout<<endl;
 s2.display();
+student s3(s2);  //calling copy constructor.(content of s2 will be copied in s3)
+cout<<endl;
+s3.display;
 }
